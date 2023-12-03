6dea1619a2890083986f431825ca04d42f5395b0
Gaurang Gunde <gaurang.gunde@thinkdeca.com>
2023-12-03 18:42:36 +0530

    refactor: Split into functions and update UI

--- Android-weather/app/src/main/java/com/weather/forecast/clearsky/mainscreen/ui/MainActivity.kt
+++ Android-weather/app/src/main/java/com/weather/forecast/clearsky/mainscreen/ui/MainActivity.kt
@@ -1,26 +1,26 @@
 package com.weather.forecast.clearsky.mainscreen.ui
 
 import android.os.Bundle
-import android.text.Editable
 import android.util.Log
-import android.view.View
-import android.widget.Toast
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.weather.forecast.clearsky.databinding.ActivityMainBinding
 import com.weather.forecast.clearsky.mainscreen.viewmodel.MainViewModel
+import com.weather.forecast.clearsky.model.WeatherImageModel
+import com.weather.forecast.clearsky.model.WeatherModel
 import com.weather.forecast.clearsky.network.ResultData
 import com.weather.forecast.clearsky.ui.theme.ClearSkyTheme
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -32,45 +32,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        binding.textView.setOnEditorActionListener { v, actionId, event ->
-            viewModel.getWeatherData(v.toString()).observe(this) {
-                when (it) {
-                    is ResultData.Success -> {
-                        Log.i("harry", "" + it.toString())
-                        it.data?.let { it1 -> Log.d("TAG", "onCreate: $it1") }
-                        viewModel.getWeatherImage(v.text.toString(), it.data!!.current.condition.text).observe(this) { itimage ->
-                            when (itimage) {
-                                is ResultData.Success -> {
-                                    Log.i("harry", "onCreateImage: $itimage")
-                                    itimage.data?.let { it1 -> Log.d("TAG", "onCreate: $it1") }
-                                    // binding.textView.setText(itimage.toString())
-                                    setImageUsingGlide(binding, itimage.data!!.image_url)
-                                }
-
-                                is ResultData.Failed -> {
-                                    Log.d("TAG", "onCreateImage: failed ${itimage.message}")
-                                }
-
-                                is ResultData.Loading -> {
-                                    Log.d("TAG", "onCreateImage: Loading")
-                                }
-                            }
-                        }
-                    }
-
-                    is ResultData.Failed -> {
-                        Log.d("TAG", "onCreate: failed ${it.message}")
-                    }
-
-                    is ResultData.Loading -> {
-
-                        Log.d("TAG", "onCreate: Loading")
-                    }
-                }
-            }
-
-            return@setOnEditorActionListener true
+        binding.textView.setOnEditorActionListener { v, actionId, _ ->
+            viewModel.getWeatherData(v.toString())
+                .observe(this, getWeatherDataObserver(v.text.toString()))
+            return@setOnEditorActionListener false
         }
+        binding.progressBar.isVisible = false
         setContentView(binding.root)
 
 //        setContent {
@@ -92,6 +59,51 @@ class MainActivity : ComponentActivity() {
             .load(imageURL)
             .centerCrop()
             .into(binding.imageView);
+    }
+
+    private fun getWeatherDataObserver (v: String): Observer<ResultData<WeatherModel>> {
+        return Observer<ResultData<WeatherModel>> ()  {
+            when (it) {
+                is ResultData.Success -> {
+                    Log.i("harry", "" + it.toString())
+                    it.data?.let { it1 -> Log.d("TAG", "onCreate: $it1") }
+                    binding.temperature.text = it.data!!.current.temp_f.toString()
+                    binding.windSpeed.text = it.data!!.current.wind_mph.toString()
+                    binding.humidity.text = it.data!!.current.humidity.toString()
+                    viewModel.getWeatherImage(v, it.data!!.current.condition.text).observe(this, getWeatherImageObserver())
+                }
+
+                is ResultData.Failed -> {
+                    Log.d("TAG", "onCreate: failed ${it.message}")
+                }
+
+                is ResultData.Loading -> {
+                    Log.d("TAG", "onCreate: Loading")
+                }
+            }
+        }
+    }
+    private fun getWeatherImageObserver (): Observer<ResultData<WeatherImageModel>> {
+        return Observer<ResultData<WeatherImageModel>> () { itimage ->
+            when (itimage) {
+                is ResultData.Success -> {
+                    Log.i("harry", "onCreateImage: $itimage")
+                    itimage.data?.let { it1 -> Log.d("TAG", "onCreate: $it1") }
+                    // binding.textView.setText(itimage.toString())
+                    setImageUsingGlide(binding, itimage.data!!.image_url)
+                    binding.progressBar.isVisible = false
+                }
+
+                is ResultData.Failed -> {
+                    Log.d("TAG", "onCreateImage: failed ${itimage.message}")
+                }
+
+                is ResultData.Loading -> {
+                    Log.d("TAG", "onCreateImage: Loading")
+                    binding.progressBar.isVisible = true
+                }
+            }
+        }
     }
 }
 
